2154a44114cf267f54803603c616145e5c0349bb
Hosmel Quintana <hosmelq@gmail.com>
2017-08-03 12:56:18 -0600

    remove prettier

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const redis = require('redis');
 let client;
 
 exports.createClient = (...args) => {
-  client = redis.createClient(...args);
+	client = redis.createClient(...args);
 
-  return client;
-}
+	return client;
+};
 
 exports.cache = (key, seconds, callback) => {
-  return new Promise((resolve, reject) => {
-    client.get(key, async (err, reply) => {
-      if (err) {
-        return reject(err);
-      }
+	return new Promise((resolve, reject) => {
+		client.get(key, async (err, reply) => {
+			if (err) {
+				return reject(err);
+			}
 
-      if (reply) {
-        return resolve(reply);
-      }
+			if (reply) {
+				return resolve(reply);
+			}
 
-      try {
-        const value = await callback();
+			try {
+				const value = await callback();
 
-        client.set(key, value, 'EX', seconds);
+				client.set(key, value, 'EX', seconds);
 
-        resolve(value);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  });
-}
+				resolve(value);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	});
+};
